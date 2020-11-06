ba5963cb1bf058129661ee3c95975ccc611261b6
barisizar <54599797+barisizar@users.noreply.github.com>
2020-11-06 10:31:19 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,37 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-class App extends Component {
-  render(){
-    return (
-      <View style={styles.container}>
-        <Image style={{width: "100%", height:300}}
-          source={require('./src/images/piedpiper.png')}
-          resizeMode="contain"
-        />
-        <Text style={{fontSize:44, fontWeight:'bold', color:'white'}} >
-        Hello</Text>
-        <Text style={{fontSize:24, color:'gray', textAlign:'center', marginHorizontal: 20}} >
-         Welcome to Pied Piper sign in to walk and win!</Text>
-        <View style={{flexDirection:'row', margin:20, paddingVertical:20}}>
-          <TouchableOpacity
-          style={{backgroundColor: '#28a628', padding:10, width:100, borderRadious:30, marginHorizontal:2, borderColor:'white'}}>
-            <Text style={{textAlign:'center', color:'white', fontsize:18}}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-          style={{backgroundColor: '#28a628', padding:10, width:100, borderRadious:30, marginHorizontal:2, borderColor:'white'}}>
-            <Text style={{textAlign:'center', color:'white', fontsize:18}}>SignUp</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-}
+import HomeScreen from './src/screens/HomeScreen.js';
+import { render } from 'react-native-web';
+
+const RootStack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={HomeScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+
+const HomeScreen = () => {
+    return(
+        <View style={styles.container}>
+        <Image style={{width: "100%", height:300}}
+        source={require('../images/piedpiper.png')}
+        resizeMode="contain"
+        />
+        <Text style={{fontSize:44, fontWeight:'bold', color:'white'}} >
+        Hello</Text>
+        <Text style={{fontSize:24, color:'gray', textAlign:'center', marginHorizontal: 20}} >
+        Welcome to Pied Piper sign in to walk and win!</Text>
+        <View style={{flexDirection:'row', margin:20, paddingVertical:20}}>
+        <TouchableOpacity
+        style={{backgroundColor: '#28a628', padding:10, width:100, borderRadious:30, marginHorizontal:2, borderColor:'white'}}>
+            <Text style={{textAlign:'center', color:'white', fontsize:18}}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+        style={{backgroundColor: '#28a628', padding:10, width:100, borderRadious:30, marginHorizontal:2, borderColor:'white'}}>
+            <Text style={{textAlign:'center', color:'white', fontsize:18}}>SignUp</Text>
+        </TouchableOpacity>
+        </View>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#4D454F',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default HomeScreen;
