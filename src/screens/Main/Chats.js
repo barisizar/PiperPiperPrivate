e3f8e39e45edf1ecfbb6dc3df31dcc4dff8eb8a9
Mertt Doğanay <MERTLOL3@outlook.com>
2021-02-07 12:17:03 +0300

    Chats added

--- src/screens/Main/Chats.js
+++ src/screens/Main/Chats.js
@@ -1,16 +1,104 @@
-import React from 'react';
-import {View,Text} from 'react-native';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, ScrollView, Text, Button, StyleSheet } from 'react-native';
+import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Chats = () => {
-    return(
-        <View style={{
-            flex:1,
-            alignItems:'center',
-            justifyContent:'center'
-        }}>
-            <Text>Chats Screen</Text>
-        </View>
-    )
-}
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello Hocam',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+            {
+                _id: 2,
+                text: 'Hello Arkadaslar',
+                createdAt: new Date(),
+                user: {
+                    _id: 1,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ]);
+    }, []);
+
+    const onSend = useCallback((messages = []) => {
+        setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, messages),
+        );
+    }, []);
+
+    const renderSend = (props) => {
+        return (
+            <Send {...props}>
+                <View>
+                    <MaterialCommunityIcons
+                        name="send-circle"
+                        style={{ marginBottom: 5, marginRight: 5 }}
+                        size={32}
+                        color="#191970"
+                    />
+                </View>
+            </Send>
+        );
+    };
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#ffd700',
+                    },
+                }}
+                textStyle={{
+                    right: {
+                        color: '#000000',
+                    },
+                }}
+            />
+        );
+    };
+
+    const scrollToBottomComponent = () => {
+        return (
+            <FontAwesome name='angle-double-down' size={22} color='#333' />
+        );
+    }
+
+    return (
+        <GiftedChat
+            messages={messages}
+            onSend={(messages) => onSend(messages)}
+            user={{
+                _id: 1,
+            }}
+            renderBubble={renderBubble}
+            alwaysShowSend
+            renderSend={renderSend}
+            scrollToBottom
+            scrollToBottomComponent={scrollToBottomComponent}
+        />
+    );
+};
+
 export default Chats;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
