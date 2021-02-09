ce1dddb06b707cf453f179be4bd84ec5298be573
baranozzgurr <54588692+baranozzgurr@users.noreply.github.com>
2021-02-09 16:36:14 +0300

    Fixing Errors in the Profile Screen

--- src/screens/Main/Profile.js
+++ src/screens/Main/Profile.js
@@ -1,68 +1,152 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
-import firebase from 'firebase';
-import 'firebase/firestore';
+import React from 'react';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from 'react-native-paper';
 
-function Profile (props) {
-    const [email, setEmail] = useState(null);
-    const [name, setName] = useState(null);
-    const [uid, setUid] = useState(null);
-    const [gender, setGender] = useState(null);
-    // useEffect(() => {
-    //     console.log(firebase.auth().currentUser.uid)
-    //     if (firebase.auth().currentUser.uid) {
-    //         setEmail(firebase.auth().currentUser.email)
-    //         setName(firebase.auth().currentUser.name)
-    //         setUid(firebase.auth().currentUser.uid)
-    //     }
-        
-    // })
-    const Info= () => {
-    //     setName(firebase.firestore().collection("users")
-    //     .doc(firebase.auth().currentUser.uid).get(name)
-    //     .then(console.log.name)
-    //     )
-      }
-  const onSignOut = () => {
-      firebase.auth().signOut()
-          .then((result) => {
-              console.log(result)
-          })
-          .catch((error) => {
-              console.log(error)
-          })
-  }
-    return(
-      <View style={{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
-    }}>
-        <Text>Profile</Text>
-        <Text>User Id : {uid} </Text>
-        <Text>Name : {name} </Text>
-        <Text>Mail : {email} </Text>
-        <TouchableOpacity 
-                onPress={() => Info()}
-                style={{marginLeft: 3, width:"45%", backgroundColor:"#042494", borderRadius:25, height:50, alignItems:"center", justifyContent:"center", marginTop:40, marginBottom:10}}>
-                    <Text style={styles.buttonText}>Info</Text>
-            </TouchableOpacity>
-        <TouchableOpacity 
-                onPress={() => onSignOut()}
-                style={{marginLeft: 3, width:"45%", backgroundColor:"#042494", borderRadius:25, height:50, alignItems:"center", justifyContent:"center", marginTop:40, marginBottom:10}}>
-                    <Text style={styles.buttonText}>Sign Out</Text>
-            </TouchableOpacity>
-    </View>
-      
-        )
+
+
+const ProfileScreen = () => {
+
+  const myCustomShare = async() => {
+    const shareOptions = {
+      message: 'Welcome to the piper!',
     }
+
+    try {
+      const ShareResponse = await Share.open(shareOptions);
+      console.log(JSON.stringify(ShareResponse));
+    } catch(error) {
+      console.log('Error => ', error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.userInfoSection}>
+        <View style={{flexDirection: 'row', marginTop: 15}}>
+          
+          <View style={{marginLeft: 20}}>
+            <Title style={[styles.title, {
+              marginTop:15,
+              marginBottom: 5,
+            }]}>John Doe</Title>
+            <Caption style={styles.caption}>@j_doe</Caption>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.userInfoSection}>
+        <View style={styles.row}>
+          <Text style={{color:"#777777", marginLeft: 20}}>Istanbul</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={{color:"#777777", marginLeft: 20}}>Age:</Text>
+        </View>
+        
+      </View>
+
+      <View style={styles.infoBoxWrapper}>
+      <View style={[styles.infoBox, {
+            borderRightColor: '#dddddd',
+            borderRightWidth: 1
+          }]}>
+            <Title>Bio:</Title>
+            <Caption>-</Caption>
+          </View>
+          <View style={styles.infoBox}>
+            <Title>12</Title>
+            <Caption>Social Media</Caption>
+          </View>
+      </View>
+
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Text style={styles.menuItemText}>Hobbies:</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Text style={styles.menuItemText}>Favorite Movies:</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={myCustomShare}>
+          <View style={styles.menuItem}>
+            <Text style={styles.menuItemText}>Interested with:</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+           
+            <Text style={styles.menuItemText}>Favorite Bands:</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            
+            <Text style={styles.menuItemText}>Settings</Text>
+          </View>
+        </TouchableRipple>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ProfileScreen;
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-export default Profile;
+  container: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: '500',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+    borderTopColor: '#dddddd',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    height: 100,
+  },
+  infoBox: {
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: '#777777',
+    marginLeft: 20,
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
